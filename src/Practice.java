420a272c66fa2d52c7bc43a07aaca16a8c3fc8c4
Matthew Vargas <matthewvargas2013@gmail.com>
2026-09-23 23:15:59 -0700

    practico

--- src/Practice.java
+++ src/Practice.java
@@ -159,7 +159,15 @@ public class Practice {
      * @return the sum of the nodes at the given level
      */
     public static int sumAtLevel(BinaryTreeNode<Integer> root, int level) {
-        return 0;
+        if(root == null) return 0;
+        int leftLevels = sumAtLevel(root.left, level - 1);
+        int rightLevels = sumAtLevel(root.right, level - 1);
+        if(level == 1){
+            return root.data;
+        }
+        else{
+            return leftLevels + rightLevels;
+        }
     }
 
 
