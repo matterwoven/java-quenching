b2e00afcb41e23feff6a87f8831e2af3f2a91168
Matthew Vargas <matthewvargas2013@gmail.com>
2026-09-23 22:08:43 -0700

    Other problem

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -103,7 +104,17 @@ public class Practice {
      * @return a frequency map of values in the list
      */
     public static <T> Map<T, Integer> frequencies(ListNode<T> head) {
-        return null;
+        Map<T, Integer> theFrequencyMap = new HashMap<>(); 
+        ListNode<T> current = head;
+
+        while(current != null){
+            theFrequencyMap.put(
+                current.data, 
+                theFrequencyMap.getOrDefault(current.data, 1)
+            );
+            current = current.next;
+        }
+        return theFrequencyMap;
     }
 
 
